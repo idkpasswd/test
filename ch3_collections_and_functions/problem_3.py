dd80c0c0232a2568bc205f3a6fc9025dbcc88c7e
Donald Mars <37024781+idkpasswd@users.noreply.github.com>
2024-10-11 21:15:00 -0700

    Initial commit

---
+++ ch3_collections_and_functions/problem_3.py
@@ -0,0 +1,15 @@
+import string #(You'll find string.punctuation useful for this problem)
+
+def fill_completions(fd):
+    pass
+
+
+def find_completions(prefix, c_dict):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ ch5_manipulating_objects/problem_2_3_4_5.py
@@ -0,0 +1,9 @@
+from numbers import Rational
+import math
+from math import gcd, lcm
+
+class Fraction:
+    pass
+
+class NotAnIntegerException:
+    pass
